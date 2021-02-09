5c14a06fb509d1fbae57a0b07fe5cca5c94b2efa
Alex Kurochkin <pin-pong-pin@mail.ru>
2021-02-09 10:41:22 +0300

    init commit

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{    
+    private float turnSpeed = 100f;
+    private float forwardSpeed = 20f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+               
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed * verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
